f21c4f1c6a3617feb0da404c38359b4a95e7a912
Walesson <walesson.silva@i-value.com.br>
2021-07-24 16:12:05 -0300

    task: config tests with jest

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
-const content = "jest test";
-console.info("content:", content);
+import React from "react";
+import ReactDom from "react-dom";
+import App from "./src";
+import "./style.scss";
+
+ReactDom.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("app")
+);
